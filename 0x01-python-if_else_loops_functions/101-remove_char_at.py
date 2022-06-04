423bcd49ae2b390d6a4b1ae60a5035eca9870288
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-06-04 13:16:08 +0100

    remove char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new = str[:n] + str[n+1:]
-    return new
+    if n >= 0:
+        new = str[:n] + str[n+1:]
+        return new
+    else:
+        return str
