5e1ecf707125ba673901f4ba84be56f7b5a3d213
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-06-06 10:26:54 +0100

    list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if not my_list:
+        return
+    my_list = my_list.copy()
     if 0<= idx < len(my_list):
         my_list[idx] = element
-        return my_list
     return my_list
