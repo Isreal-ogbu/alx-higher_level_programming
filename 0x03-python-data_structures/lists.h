a07d874f63768930e5da54f2c5bf2c969cbb522a
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-06-06 13:18:20 +0100

    interview

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,8 +1,26 @@
-#!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    # If too long, cut the tuple to the first 2 elements
-    # if too short, extend the tuple to match length 2
-    a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
-    b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
-    c = [x + y for x, y in zip(a, b)]
-    return tuple(c[0:2])
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
