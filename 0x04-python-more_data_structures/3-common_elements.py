cd117e23f11d4beb41e3f7e2d947dbe9a99546c9
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-06-16 11:36:31 +0100

    common element

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1=list(set_1)
-    set_2 = list(set_2)
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                return i
+    set_3 = set_1 & set_2
+    return set_3
