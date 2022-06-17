1f9e86e646dbf19b196eca208431566416176a8a
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-06-17 14:11:17 +0100

    multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    for i in sorted(a_dictionary.keys()):
-        print("{}: {}".format(i, (a_dictionary[i])*2))
+def multiply_by_2(my_dict):
+    return {key: val*2 for key, val in my_dict.items()}
