585d4e6ed6e24d283a680e42da73cba4cfce923e
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-06-17 13:10:31 +0100

    sorted dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    for i in sorted(a_dictionary.keys()):
-        if i == key:
-            a_dictionary[i] = value
-            print("{}: {}".format(i, a_dictionary[i]))
-        else:
-            print("{}: {}".format(i, a_dictionary[i]))
-
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
