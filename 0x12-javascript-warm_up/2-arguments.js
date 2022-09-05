a9b31bdbed7012678f97f35cb284147c8fe6a709
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-05 14:29:10 +0100

    printing strings

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const arg = process.argv.length - 2;
-if (arg === 0) {
-    console.log('No argument');
-} else if (arg === 1) {
-    console.log('Argument found');
+const numArgs = process.argv.length - 2;
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
