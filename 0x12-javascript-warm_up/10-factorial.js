3c4358ca78eba58fe4faf75804ed8d15b792a075
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-05 17:31:52 +0100

    recursion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let factorial = parseInt(process.argv[2]);
 function fac(n) {
-    if (isNaN(a)){
+    if (isNaN(n)){
         return 1
     } if (n == 1) {
         return n
@@ -9,3 +9,4 @@ function fac(n) {
         return n * fac(n-1)
     }
 }
+console.log(fac(factorial))
