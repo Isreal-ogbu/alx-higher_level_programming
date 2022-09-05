f48b07cd0920fb147f02cd2c9661d87773d4b14b
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-05 15:12:34 +0100

    loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const firstArg = parseInt(process.argv[2]);
-if (isNaN(firstArg)) {
-    console.log('Missing number of occurrences');
+const times = parseInt(process.argv[2]);
+if (isNaN(times)) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < firstArg; i++) {
-        console.log('C is fun');
-    }
-};
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+}
