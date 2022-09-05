1d5f56e58d530cab5258319d2108a522cc562029
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-05 16:43:52 +0100

    loops

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const firstArg = parseInt(process.argv);
-function add(a,b) {
+function add(a, b) {
     if (isNaN(a) || isNaN(b)) {
         return NaN;
     } else {
         return a + b;
     }
 }
-const val = add(firstArg[2], firstArg[3]);
-console.log(val)
+console.log(add(firstArg[2], firstArg[3]));
