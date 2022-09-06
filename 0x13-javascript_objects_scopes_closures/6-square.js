7fb39a851454680e565a627bb4e511aec69ff683
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-06 18:29:04 +0100

    classes in Javascripts

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,15 +5,11 @@ module.exports = class Square extends SquareModel {
   constructor (size) {
     super(size, size);
   }
-  charPrint (c) {
-    if (c == undefined) {
-        for (let i = 0; i < this.size.length; i++) {
-            console.log('X'.repeat(this.size))
-        }
-    } else {
-        for (let i = 0; i < this.size.length; i++) {
-            console.log(c.repeat(this.size))
-        }
+
+  charPrint (c = '') {
+    if (c) {
+        this.print(c);
     }
+    this.print();
   }
 };
