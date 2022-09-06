47cf9017a70985a2616aac672978337924c769d1
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-06 18:40:22 +0100

    classes in Javascripts

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 module.exports = function nbOccurences(list, val) {
     let count = 0;
     for (let j = 0; j < list.length; j++) {
-        if (list[j] == val){
+        if (list[j] === val){
             count ++
         }
     } return count
