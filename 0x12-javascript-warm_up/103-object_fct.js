2e7ecdd6e93fe30850d8809e67fa22b45c51678b
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-06 13:42:46 +0100

    global functions

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
 const myObject = {
     type: 'object',
-    value: 12
+    value: 12,
+    incr() {
+        let b = 1;
+        myObject.value = b + myObject.value
+    }
 };
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
 console.log(myObject);
