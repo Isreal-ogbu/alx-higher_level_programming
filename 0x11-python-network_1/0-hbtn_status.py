eb6c574965dc496d5cc1cc7893beac8b3a16e3c9
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-24 22:40:40 +0100

    python-network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a Python script that fetches https://alx-intranet.hbtn.io/status
-You must use the package urllib
-You are not allowed to import any packages other than urllib
-The body of the response must be displayed like the following example (tabulation before -)
-You must use a with statement
-"""
+"""fetches https://intranet.hbtn.io/status"""
 from urllib import request
 
 if __name__ == "__main__":
