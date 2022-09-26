abd7ad91e6e089261c338c2f21c3460c3eecc52b
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-26 07:06:05 +0100

    python-network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Write a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sfL "$1" -X POST
+# Send JSON data
+curl -s -d "@$2" -X POST -H 'Content-Type: application/json' $1
