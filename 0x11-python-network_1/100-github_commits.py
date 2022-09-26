97e243a7bc6ae3c8806a32340f2ae168c2227623
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-09-26 07:27:57 +0100

    python-network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Arguments: arg1: repo name, arg2: repo owner
+"""
+Python script that shows the last 10 commits of a repository
+in GitHub
 """
 import sys
 import requests
-
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    # Get the arguments, arg1: repo name, arg2: repo owner
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[2], sys.argv[1])
-
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
-    except IndexError:
-        pass
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{:s}/{:s}/commits".format(
+        owner, repo)
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        name = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, name))
