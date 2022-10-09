21098be8dc939c0b568649c489376b429372e905
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-10-09 21:48:08 +0100

    select state

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,1 +1,2 @@
-CREATE TABLE IF NOT EXISTS force_name (id int, name VARCHAR(256) NOT NULL);
+-- creates the table force_name
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
