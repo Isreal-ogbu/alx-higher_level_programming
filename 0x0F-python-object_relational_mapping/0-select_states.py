8187d0050d3df392f726585be53bffddfed0ab2f
Ogbu Isreal <ogbuisreal22@gmail.com>
2022-10-09 20:15:44 +0100

    select state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
         port=3306,
         host='localhost')
     cursor = db.cursor()
-    cursor.execute('SELECT states.id, ststes.name FROM states ORDER BY states.id ASC')
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
 
     states = cursor.fetchall()
     for state in states:
-        print(f"({state[0]}, {state[1]})")
+        print(f"({state[0]}, '{state[1]}')")
